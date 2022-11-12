688c4a123d1c9e5de182d2048de7ea957cf3bb8c
yabro <yabrow@yandex.ru>
2022-11-12 20:11:16 +0500

    feat: updated serialization annotations.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -11,29 +14,34 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Data
 @NotNull
-public class Film implements Serializable {
+public class Film {
 
     private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    private final static long serialVersionUID = 0L;
+    @JsonCreator
+    public Film() {
+    }
 
+    @JsonProperty("id")
     private Integer id;
 
     @NotNull
     @NotBlank
+    @JsonProperty("name")
     private String name;
 
     @NotNull
     @Size(max = 200)
+    @JsonProperty("description")
     private String description;
 
     @NotNull
+    @JsonProperty("release_date")
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
@@ -41,8 +49,10 @@ public class Film implements Serializable {
 
     @NotNull
     @Positive
+    @JsonProperty("duration")
     private Integer duration;
 
+    @JsonIgnore
     public String getReleaseDateString() {
         return releaseDate.format(formatter);
     }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
@@ -11,29 +13,35 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @NotNull
-public class User implements Serializable {
+public class User {
 
-    private final static long serialVersionUID = 1L;
+    @JsonCreator
+    public User() {
+    }
 
+    @JsonProperty("id")
     private Integer id;
 
     @Email
     @NotNull
     @NotBlank
+    @JsonProperty("email")
     private String email;
 
     @NotNull
     @NotBlank
+    @JsonProperty("login")
     private String login;
 
+    @JsonProperty("name")
     private String name;
 
     @Past
+    @JsonProperty("birthday")
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
