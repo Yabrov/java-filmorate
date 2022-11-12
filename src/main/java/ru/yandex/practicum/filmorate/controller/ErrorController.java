e47bc0d25b77727bf27ab83781ba7bef527d4954
yabro <yabrow@yandex.ru>
2022-11-12 20:12:26 +0500

    feat: added global exception & validation exception handlers.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -1,37 +1,54 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import ru.yandex.practicum.filmorate.exception.FilmNotFound;
-import ru.yandex.practicum.filmorate.exception.UserNotFound;
-import ru.yandex.practicum.filmorate.exception.WrongFilmReleaseDate;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.WrongFilmReleaseDateException;
 
 @ControllerAdvice
 public class ErrorController {
 
     @ResponseBody
-    @ExceptionHandler(WrongFilmReleaseDate.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Film wrongFilmReleaseDate(WrongFilmReleaseDate e) {
-        return e.getFilm();
+    public String genericExceptionHandler(Exception e) {
+        return e.getMessage();
     }
 
     @ResponseBody
-    @ExceptionHandler(FilmNotFound.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Film filmNotFound(FilmNotFound e) {
-        return e.getFilm();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String validationExceptionHandler(MethodArgumentNotValidException e) {
+        FieldError error = e.getFieldErrors().get(0);
+        return "Validation exception [class: '" + error.getObjectName()
+                + "', field: '" + error.getField() + "', reason: '"
+                + error.getDefaultMessage() + "']";
     }
 
     @ResponseBody
-    @ExceptionHandler(UserNotFound.class)
+    @ExceptionHandler(WrongFilmReleaseDateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String wrongFilmReleaseDate(WrongFilmReleaseDateException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(FilmNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public User userNotFound(UserNotFound e) {
-        return e.getUser();
+    public String filmNotFound(FilmNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String userNotFound(UserNotFoundException e) {
+        return e.getMessage();
     }
 }
