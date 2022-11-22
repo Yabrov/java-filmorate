807614937d4ac112bb51a488152a23868c176587
yabro <yabrow@yandex.ru>
2022-11-22 17:11:25 +0500

    feat: validation errors handler updated.

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -1,54 +1,103 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
-import ru.yandex.practicum.filmorate.exception.WrongFilmReleaseDateException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.OffsetDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
 @ControllerAdvice
-public class ErrorController {
+public class ErrorController extends ResponseEntityExceptionHandler {
 
-    @ResponseBody
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String genericExceptionHandler(Exception e) {
-        return e.getMessage();
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+    private static final String ERROR = "error";
+    private static final String PATH = "path";
+    private static final String REASONS = "reasons";
+    private static final String OBJECT = "object";
+
+    @ExceptionHandler(value = FilmNotFoundException.class)
+    protected ResponseEntity<Object> filmNotFound(FilmNotFoundException ex, WebRequest request) {
+        log.error("Фильм с id={} не найден.", ex.getFilm().getId());
+        Map<String, Object> body = getGeneralErrorBody(HttpStatus.NOT_FOUND, request);
+        body.put(OBJECT, ex.getFilm());
+        body.put(REASONS, Collections.singletonList(ex.getMessage()));
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
-    @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String validationExceptionHandler(MethodArgumentNotValidException e) {
-        FieldError error = e.getFieldErrors().get(0);
-        return "Validation exception [class: '" + error.getObjectName()
-                + "', field: '" + error.getField() + "', reason: '"
-                + error.getDefaultMessage() + "']";
+    @ExceptionHandler(value = UserNotFoundException.class)
+    protected ResponseEntity<Object> userNotFound(UserNotFoundException ex, WebRequest request) {
+        log.error("Пользователь с id={} не найден.", ex.getUser().getId());
+        Map<String, Object> body = getGeneralErrorBody(HttpStatus.NOT_FOUND, request);
+        body.put(OBJECT, ex.getUser());
+        body.put(REASONS, Collections.singletonList(ex.getMessage()));
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
-    @ResponseBody
-    @ExceptionHandler(WrongFilmReleaseDateException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String wrongFilmReleaseDate(WrongFilmReleaseDateException e) {
-        return e.getMessage();
+    @ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<Object> genericExceptionHandler(Exception ex, WebRequest request) {
+        log.error("Внутренняя ошибка сервера.", ex);
+        Map<String, Object> body = getGeneralErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, request);
+        body.put(REASONS, ex.getMessage());
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
-    @ResponseBody
-    @ExceptionHandler(FilmNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String filmNotFound(FilmNotFoundException e) {
-        return e.getMessage();
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        log.error("Validation failed. Message: {}", ex.getMessage(), ex);
+        Map<String, Object> body = getGeneralErrorBody(status, request);
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(this::getErrorString).
+                collect(Collectors.toList());
+        body.put(REASONS, errors);
+        return new ResponseEntity<>(body, headers, status);
     }
 
-    @ResponseBody
-    @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String userNotFound(UserNotFoundException e) {
-        return e.getMessage();
+    private Map<String, Object> getGeneralErrorBody(HttpStatus status,
+                                                    WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, OffsetDateTime.now());
+        body.put(STATUS, status.value());
+        body.put(ERROR, status.getReasonPhrase());
+        body.put(PATH, getRequestURI(request));
+        return body;
+    }
+
+    private String getErrorString(ObjectError error) {
+        if (error instanceof FieldError) {
+            return "Validation exception [class: '" + error.getObjectName()
+                    + "', field: '" + ((FieldError) error).getField() + "', mes: '"
+                    + error.getDefaultMessage() + "']";
+        }
+        return error.getDefaultMessage();
+    }
+
+    private String getRequestURI(WebRequest request) {
+        if (request instanceof ServletWebRequest) {
+            HttpServletRequest requestHttp = ((ServletWebRequest) request).getRequest();
+            return String.format("%s %s", requestHttp.getMethod(), requestHttp.getRequestURI());
+        } else {
+            return "";
+        }
     }
 }
