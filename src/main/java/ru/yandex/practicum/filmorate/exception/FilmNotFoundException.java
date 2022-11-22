ba3d46b45a7b6578f01d35fdec0f9332aaddcaf9
yabro <yabrow@yandex.ru>
2022-11-22 17:09:58 +0500

    feat: returned film object field in error class.

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -6,7 +6,10 @@ import ru.yandex.practicum.filmorate.model.Film;
 @Getter
 public class FilmNotFoundException extends RuntimeException {
 
+    private final Film film;
+
     public FilmNotFoundException(Film film) {
         super("Film with id=" + film.getId() + " doesn't exist.");
+        this.film = film;
     }
 }
