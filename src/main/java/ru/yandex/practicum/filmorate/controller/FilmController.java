ef681a2988a50ac4342b25c8224ffdd830d08bfe
yabro <yabrow@yandex.ru>
2022-11-26 19:43:54 +0500

    feat: endpoint logic has been moved to service.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,44 +1,35 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
-@Slf4j
 @RestController
 public class FilmController {
 
-    private final Map<Integer, Film> films = new HashMap<>();
+    private final FilmService filmService;
 
-    private Integer nextId = 1;
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @PostMapping(value = "/films", produces = MediaType.APPLICATION_JSON_VALUE)
     public Film addFilm(@Valid @RequestBody Film film) {
-        film.setId(nextId++);
-        log.info("{} created.", film);
-        films.put(film.getId(), film);
-        return film;
+        return filmService.createFilm(film);
     }
 
     @PutMapping(value = "/films", produces = MediaType.APPLICATION_JSON_VALUE)
     public Film updateFilm(@Valid @RequestBody Film film) {
-        if (film.getId() == null || !films.containsKey(film.getId())) {
-            throw new FilmNotFoundException(film);
-        }
-        log.info("Фильм с id={} успешно обновлен.", film.getId());
-        films.replace(film.getId(), film);
-        return film;
+        return filmService.updateFilm(film);
     }
 
     @GetMapping(value = "/films", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<Film> getAllFilms() {
-        return films.values();
+        return filmService.getAllFilms();
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,52 +1,37 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Slf4j
 @RestController
 public class UserController {
 
-    private final Map<Integer, User> users = new HashMap<>();
+    private final UserService userService;
 
-    private Integer nextId = 1;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public User createUser(@Valid @RequestBody User user) {
-        user.setId(nextId++);
-        if (isBlank(user.getName())) {
-            user.setName(user.getLogin());
-        }
-        log.info("{} created.", user);
-        users.put(user.getId(), user);
-        return user;
+        return userService.createUser(user);
     }
 
     @PutMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public User updateUser(@Valid @RequestBody User user) {
-        if (user.getId() == null || !users.containsKey(user.getId())) {
-            throw new UserNotFoundException(user);
-        }
-        if (isBlank(user.getName())) {
-            user.setName(user.getLogin());
-        }
-        log.info("Пользователь с id={} успешно обновлен.", user.getId());
-        users.replace(user.getId(), user);
-        return user;
+        return userService.updateUser(user);
     }
 
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<User> getAllUsers() {
-        return users.values();
+        return userService.getAllUsers();
     }
 }
