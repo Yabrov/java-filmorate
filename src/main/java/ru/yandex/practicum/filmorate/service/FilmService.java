01876920b6268e6ca220f82885602d8bb520099d
yabro <yabrow@yandex.ru>
2022-11-30 17:55:49 +0500

    feat: added new functions and film storage injected

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,38 +1,77 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class FilmService {
 
-    private final Map<Integer, Film> films = new HashMap<>();
+    private final static Comparator<Film> filmPopularityComparator = Comparator
+            .comparing((Film film) -> film.getLikes().size()).reversed();
 
-    private Integer nextId = 1;
+    private final FilmStorage filmStorage;
+    private final UserService userService;
 
-    public Film createFilm(Film film) {
-        Film newFilm = film.withId(nextId++);
-        log.info("{} created.", newFilm);
-        films.put(newFilm.getId(), newFilm);
-        return newFilm;
+    @Autowired
+    public FilmService(FilmStorage filmStorage, UserService userService) {
+        this.filmStorage = filmStorage;
+        this.userService = userService;
     }
 
-    public Film updateFilm(Film film) {
-        if (film.getId() == null || !films.containsKey(film.getId())) {
-            throw new FilmNotFoundException(film);
+    public Film getFilmById(Integer filmId) {
+        Film film = filmStorage.getFilm(filmId);
+        if (film == null) {
+            throw new FilmNotFoundException(filmId);
         }
-        log.info("Фильм с id={} успешно обновлен.", film.getId());
-        films.replace(film.getId(), film);
         return film;
     }
 
+    public Film createFilm(Film film) {
+        return filmStorage.persistFilm(film);
+    }
+
+    public Film updateFilm(Film film) {
+        if (film.getId() == null) {
+            throw new FilmNotFoundException(film);
+        }
+        return filmStorage.replaceFilm(film);
+    }
+
     public Iterable<Film> getAllFilms() {
-        return films.values();
+        return filmStorage.getAllFilms();
+    }
+
+    public Film addLike(Integer userId, Integer filmId) {
+        User user = userService.getUserById(userId);
+        Film film = getFilmById(filmId);
+        log.info("Пользователь с id={} лайкнул фильм '{}'.", user.getId(), film.getName());
+        film.getLikes().add(userId);
+        return film;
+    }
+
+    public Film removeLike(Integer userId, Integer filmId) {
+        User user = userService.getUserById(userId);
+        Film film = getFilmById(filmId);
+        log.info("Пользователь с id={} убрал лайк с фильма '{}'.", user.getId(), film.getName());
+        film.getLikes().remove(userId);
+        return film;
+    }
+
+    public Iterable<Film> getMostPopularFilms(int count) {
+        return filmStorage
+                .getAllFilms()
+                .stream()
+                .sorted(filmPopularityComparator)
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
