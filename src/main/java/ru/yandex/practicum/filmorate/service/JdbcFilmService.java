e03918b486ffac44c8667c7944121768eb767eba
TOP_GUN <yabrow@yandex.ru>
2023-01-04 09:33:33 +0500

    feat: added functionality

--- src/main/java/ru/yandex/practicum/filmorate/service/JdbcFilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/JdbcFilmService.java
@@ -1,41 +1,60 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.AbstractRepository;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class JdbcFilmService implements AbstractFilmService {
+
+    private final AbstractRepository<Integer, Film> jdbcFilmRepository;
 
     @Override
     public Film getFilmById(Integer filmId) {
-        return null;
+        Film film = jdbcFilmRepository.findById(filmId);
+        if (film == null) {
+            throw new FilmNotFoundException(filmId);
+        }
+        return film;
     }
 
     @Override
     public Film createFilm(Film film) {
-        return null;
+        return jdbcFilmRepository.save(film);
     }
 
     @Override
     public Film updateFilm(Film film) {
-        return null;
+        if (film.getId() == null) {
+            throw new FilmNotFoundException(film);
+        }
+        return jdbcFilmRepository.update(film);
     }
 
     @Override
     public Iterable<Film> getAllFilms() {
-        return null;
+        return jdbcFilmRepository.findAll();
     }
 
     @Override
     public Film addLike(Integer userId, Integer filmId) {
+        // TODO: 04.01.2023
         return null;
     }
 
     @Override
     public Film removeLike(Integer userId, Integer filmId) {
+        // TODO: 04.01.2023
         return null;
     }
 
     @Override
     public Iterable<Film> getMostPopularFilms(int count) {
-        return null;
+        return jdbcFilmRepository.findFirstNTopRows(count);
     }
 }
