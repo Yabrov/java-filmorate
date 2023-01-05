60b72e19c558e0401d2f36c37a436cf513dd401e
yabro <yabrow@yandex.ru>
2023-01-05 21:32:25 +0500

    fix: fixed constructor

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -6,27 +6,36 @@ import lombok.Builder;
 import lombok.Value;
 import lombok.With;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Value
 @Builder
-@NotNull
 public class Genre {
 
     @With
     Integer id;
 
     @With
-    @NotNull
-    @NotBlank
     String name;
 
     @JsonCreator
     public Genre(
             @JsonProperty(value = "id", required = true) Integer id,
-            @JsonProperty(value = "name", defaultValue = "Мультфильм") String name) {
+            @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Genre)) return false;
+        Genre genre = (Genre) o;
+        return Objects.equals(id, genre.id) && Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
