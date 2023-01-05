c72e49412965a29adb5348093f177ab8dd97bf6f
yabro <yabrow@yandex.ru>
2023-01-05 21:40:21 +0500

    feat: added functionality

--- src/main/java/ru/yandex/practicum/filmorate/service/JdbcFilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/JdbcFilmService.java
@@ -5,7 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Likes;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.AbstractRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -13,6 +18,8 @@ import ru.yandex.practicum.filmorate.storage.AbstractRepository;
 public class JdbcFilmService implements AbstractFilmService {
 
     private final AbstractRepository<Integer, Film> jdbcFilmRepository;
+    private final AbstractRepository<Likes, Likes> jdbcLikesRepository;
+    private final AbstractUserService jdbcUserService;
 
     @Override
     public Film getFilmById(Integer filmId) {
@@ -25,12 +32,13 @@ public class JdbcFilmService implements AbstractFilmService {
 
     @Override
     public Film createFilm(Film film) {
-        return jdbcFilmRepository.save(film);
+        Film createdFilm = jdbcFilmRepository.save(film);
+        return updateFilm(createdFilm);
     }
 
     @Override
     public Film updateFilm(Film film) {
-        if (film.getId() == null) {
+        if (film.getId() == null || getFilmById(film.getId()) == null) {
             throw new FilmNotFoundException(film);
         }
         return jdbcFilmRepository.update(film);
@@ -43,18 +51,55 @@ public class JdbcFilmService implements AbstractFilmService {
 
     @Override
     public Film addLike(Integer userId, Integer filmId) {
-        // TODO: 04.01.2023
-        return null;
+        User user = jdbcUserService.getUserById(userId);
+        Film film = getFilmById(filmId);
+        Likes like = new Likes(userId, filmId);
+        if (jdbcLikesRepository.findById(like) == null) {
+            jdbcLikesRepository.save(like);
+            film.getLikedUsers().add(user.getId());
+            log.info("User with id={} has liked film '{}'.", user.getId(), film.getName());
+        } else {
+            log.info("User with id={} already has liked film '{}'.", user.getId(), film.getName());
+        }
+        return film;
     }
 
     @Override
     public Film removeLike(Integer userId, Integer filmId) {
-        // TODO: 04.01.2023
-        return null;
+        User user = jdbcUserService.getUserById(userId);
+        Film film = getFilmById(filmId);
+        Likes like = new Likes(userId, filmId);
+        if (jdbcLikesRepository.delete(like) != null) {
+            log.info("User with id={} has removed like from film '{}'.", user.getId(), film.getName());
+            film.getLikedUsers().remove(user.getId());
+        } else {
+            log.info("User with id={} has not liked film '{}'.", user.getId(), film.getName());
+        }
+        return film;
     }
 
     @Override
     public Iterable<Film> getMostPopularFilms(int count) {
-        return jdbcFilmRepository.findFirstNTopRows(count);
+        Collection<Film> selectedPopular = jdbcFilmRepository
+                .findFirstNTopRows(count);
+        if (selectedPopular.isEmpty()) {
+            return jdbcFilmRepository
+                    .findAll()
+                    .stream()
+                    .limit(count)
+                    .collect(Collectors.toList());
+        } else {
+            return selectedPopular;
+        }
+    }
+
+    @Override
+    public Integer deleteFilm(Film film) {
+        Film deletedFilm = jdbcFilmRepository.delete(film);
+        if (deletedFilm == null) {
+            throw new FilmNotFoundException(film);
+        } else {
+            return film.getId();
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/JdbcUserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/JdbcUserService.java
@@ -1,46 +1,111 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.model.Friends;
+import ru.yandex.practicum.filmorate.model.FriendsStatus;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.AbstractRepository;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class JdbcUserService implements AbstractUserService {
+
+    private final AbstractRepository<Integer, User> jdbcUserRepository;
+    private final AbstractRepository<Friends, Friends> jdbcFriendsRepository;
 
     @Override
     public User getUserById(Integer userId) {
-        return null;
+        User user = jdbcUserRepository.findById(userId);
+        if (user == null) {
+            throw new UserNotFoundException(userId);
+        }
+        return user;
     }
 
     @Override
     public User createUser(User user) {
-        return null;
+        return isBlank(user.getName())
+                ? jdbcUserRepository.save(user.withName(user.getLogin()))
+                : jdbcUserRepository.save(user);
     }
 
     @Override
     public User updateUser(User user) {
-        return null;
+        if (user.getId() == null || getUserById(user.getId()) == null) {
+            throw new UserNotFoundException(user);
+        }
+        return isBlank(user.getName())
+                ? jdbcUserRepository.update(user.withName(user.getLogin()))
+                : jdbcUserRepository.update(user);
     }
 
     @Override
     public Iterable<User> getAllUsers() {
-        return null;
+        return jdbcUserRepository.findAll();
     }
 
     @Override
     public User addFriend(Integer friendId, Integer userId) {
-        return null;
+        if (friendId.equals(userId)) {
+            throw new IllegalArgumentException("User must not be friend of himself.");
+        }
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
+        Friends friends = new Friends(userId, friendId, FriendsStatus.REQUESTED);
+        if (jdbcFriendsRepository.findById(friends) == null) {
+            jdbcFriendsRepository.save(friends);
+            user.getFriends().add(friend.getId());
+            log.info("User with id {} adds friend with id {}.", userId, friendId);
+        } else {
+            log.info("User with id {} already has friend with id {}.", userId, friendId);
+        }
+        return user;
     }
 
     @Override
     public User removeFriend(Integer friendId, Integer userId) {
-        return null;
+        User user = getUserById(userId);
+        User friend = getUserById(friendId);
+        Friends friends = new Friends(userId, friendId, FriendsStatus.REQUESTED);
+        if (jdbcFriendsRepository.delete(friends) != null) {
+            log.info("User with id {} removes friend with id {}.", userId, friendId);
+            user.getFriends().remove(friend.getId());
+        } else {
+            log.info("Users with id {} has not friend with id {}", userId, friendId);
+        }
+        return user;
     }
 
     @Override
     public Iterable<User> getMutualFriends(Integer id, Integer otherId) {
-        return null;
+        User user = getUserById(id);
+        User otherUser = getUserById(otherId);
+        Set<Integer> mutualFriendsSet = new HashSet<>(user.getFriends());
+        mutualFriendsSet.retainAll(otherUser.getFriends());
+        return jdbcUserRepository.findByIds(mutualFriendsSet);
     }
 
     @Override
     public Iterable<User> getUserFriends(Integer userId) {
-        return null;
+        return jdbcUserRepository.findByIds(getUserById(userId).getFriends());
+    }
+
+    @Override
+    public Integer deleteUser(User user) {
+        User deletedUser = jdbcUserRepository.delete(user);
+        if (deletedUser == null) {
+            throw new UserNotFoundException(user);
+        } else {
+            return deletedUser.getId();
+        }
     }
 }
