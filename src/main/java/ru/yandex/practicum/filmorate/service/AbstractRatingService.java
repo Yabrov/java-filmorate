6106be2f12ee09a37157c4276a327fa02a9b82ef
yabro <yabrow@yandex.ru>
2023-01-05 21:31:11 +0500

    feat: added new abstract repositories

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/AbstractRatingService.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.service;
+
+import ru.yandex.practicum.filmorate.model.Rating;
+
+public interface AbstractRatingService {
+
+    Rating getRatingById(Integer ratingId);
+
+    Rating createRating(Rating rating);
+
+    Rating updateRating(Rating rating);
+
+    Iterable<Rating> getAllRatings();
+
+    Integer deleteRating(Integer rating);
+}
