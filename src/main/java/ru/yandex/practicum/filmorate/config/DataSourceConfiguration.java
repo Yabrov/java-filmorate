693563914e700db22f2a5a6171423dada486c7a7
yabro <yabrow@yandex.ru>
2023-01-05 21:32:59 +0500

    fix: using autowiring with autoconfigured datasource

--- src/main/java/ru/yandex/practicum/filmorate/config/DataSourceConfiguration.java
+++ src/main/java/ru/yandex/practicum/filmorate/config/DataSourceConfiguration.java
@@ -1,36 +1,18 @@
 package ru.yandex.practicum.filmorate.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("ru.yandex.practicum.filmorate")
 public class DataSourceConfiguration {
 
-    @Value("${spring.datasource.url}")
-    private String JDBC_URL;
-
-    @Value("${spring.datasource.username}")
-    private String JDBC_USERNAME;
-
-    @Value("${spring.datasource.password}")
-    private String JDBC_PASSWORD;
-
-    @Value("${spring.datasource.driverClassName}")
-    private String JDBC_DRIVER;
-
     @Bean
-    public JdbcTemplate getTemplate() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(JDBC_DRIVER);
-        dataSource.setUrl(JDBC_URL);
-        dataSource.setUsername(JDBC_USERNAME);
-        dataSource.setPassword(JDBC_PASSWORD);
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        return jdbcTemplate;
+    public JdbcTemplate getTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
