70cc9fa420396afb51b8dd09bcdffb3f1715b671
yabro <yabrow@yandex.ru>
2023-01-06 14:48:21 +0500

    fix: deleted equals & hashcode override

--- src/main/java/ru/yandex/practicum/filmorate/model/Rating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Rating.java
@@ -6,8 +6,6 @@ import lombok.Builder;
 import lombok.Value;
 import lombok.With;
 
-import java.util.Objects;
-
 @Value
 @Builder
 public class Rating {
@@ -20,22 +18,14 @@ public class Rating {
 
     @JsonCreator
     public Rating(
-            @JsonProperty(value = "id", required = true) Integer id,
-            @JsonProperty("name") String name) {
+            @JsonProperty(
+                    value = "id",
+                    required = true
+            )
+            Integer id,
+            @JsonProperty("name")
+            String name) {
         this.id = id;
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Rating)) return false;
-        Rating rating = (Rating) o;
-        return Objects.equals(id, rating.id) && Objects.equals(name, rating.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
     }
 }
