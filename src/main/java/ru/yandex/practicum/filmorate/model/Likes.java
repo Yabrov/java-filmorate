6af443d822c85a84c46b3303f5c39b29389c842b
yabro <yabrow@yandex.ru>
2023-01-07 19:54:18 +0500

    fix: deleted constructor

--- src/main/java/ru/yandex/practicum/filmorate/model/Likes.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Likes.java
@@ -1,26 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Value;
 import lombok.With;
 
 import javax.validation.constraints.NotNull;
 
+@With
 @Value
-@Builder
 @NotNull
 public class Likes {
 
-    @With
     @NotNull
     Integer userId;
 
-    @With
     @NotNull
     Integer filmId;
-
-    public Likes(Integer userId, Integer filmId) {
-        this.userId = userId;
-        this.filmId = filmId;
-    }
 }
