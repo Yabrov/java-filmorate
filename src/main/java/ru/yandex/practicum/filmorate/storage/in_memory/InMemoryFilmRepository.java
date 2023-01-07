ebadfae5e342a05eaace2d07f7dc5dd3be453ff9
yabro <yabrow@yandex.ru>
2023-01-08 00:42:34 +0500

    feat: completely changed

--- src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryFilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryFilmRepository.java
@@ -1,72 +1,166 @@
 package ru.yandex.practicum.filmorate.storage.in_memory;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Likes;
+import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.storage.AbstractRepository;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Repository
 public class InMemoryFilmRepository implements AbstractRepository<Integer, Film> {
 
-    private final Map<Integer, Film> films = new HashMap<>();
+    private static final Comparator<Film> filmPopularityComparator = Comparator
+            .comparing((Film film) -> film.getLikedUsers().size()).reversed();
 
+    private final Map<Integer, FilmInfo> films = new HashMap<>();
+
+    private final AbstractRepository<Likes, Likes> likesRepository;
+    private final AbstractRepository<Integer, Genre> genreRepository;
+    private final AbstractRepository<Integer, Rating> ratingRepository;
     private Integer nextId = 1;
+
+    @Autowired
+    public InMemoryFilmRepository(
+            @Qualifier("inMemoryLikesRepository") AbstractRepository<Likes, Likes> likesRepository,
+            @Qualifier("inMemoryGenreRepository") AbstractRepository<Integer, Genre> genreRepository,
+            @Qualifier("inMemoryRatingRepository") AbstractRepository<Integer, Rating> ratingRepository) {
+        this.likesRepository = likesRepository;
+        this.genreRepository = genreRepository;
+        this.ratingRepository = ratingRepository;
+    }
 
     @Override
     public Film save(Film film) {
-        Film persistedFilm = film.withId(getNextId());
+        Integer newId = getNextId();
+        FilmInfo persistedFilm = new FilmInfo(film.withId(newId));
         log.info("{} created.", persistedFilm);
         films.put(persistedFilm.getId(), persistedFilm);
-        return persistedFilm;
+        return film.withId(newId);
     }
 
     @Override
     public Film update(Film film) {
-        Film oldFilm = films.get(film.getId());
+        FilmInfo oldFilm = films.get(film.getId());
         if (oldFilm == null) {
             throw new FilmNotFoundException(film);
         }
         log.info("Film with id={} has been updated.", film.getId());
-        film.getLikedUsers().clear();
-        film.getLikedUsers().addAll(oldFilm.getLikedUsers());
-        films.replace(film.getId(), oldFilm, film);
-        return film;
+        films.replace(film.getId(), oldFilm, new FilmInfo(film));
+        return findById(film.getId());
     }
 
     @Override
     public Film delete(Film film) {
-        return films.remove(film.getId());
+        Film deletedFilm = findById(film.getId());
+        if (deletedFilm != null) {
+            deletedFilm.getLikedUsers().forEach(id -> likesRepository
+                    .delete(new Likes(id, film.getId())));
+            films.remove(film.getId());
+        }
+        return deletedFilm;
     }
 
     @Override
     public Film findById(Integer id) {
-        return films.get(id);
+        FilmInfo film = films.get(id);
+        if (film != null) {
+            return getFilmFromFilmInfo(film);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Collection<Film> findAll() {
-        return films.values();
+        return films
+                .values()
+                .stream()
+                .map(this::getFilmFromFilmInfo)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Film> findByIds(Collection<Integer> ids) {
-        return ids.stream().map(this::findById).collect(Collectors.toList());
+        return ids
+                .stream()
+                .map(this::findById)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Film> findFirstNTopRows(Integer n) {
-        return Collections.emptyList();
+        return findAll()
+                .stream()
+                .sorted(filmPopularityComparator)
+                .limit(n)
+                .collect(Collectors.toList());
     }
 
     private synchronized Integer getNextId() {
         return nextId++;
+    }
+
+    private Film getFilmFromFilmInfo(FilmInfo filmInfo) {
+        Film film = Film.builder()
+                .id(filmInfo.getId())
+                .name(filmInfo.name)
+                .description(filmInfo.description)
+                .releaseDate(filmInfo.releaseDate)
+                .duration(filmInfo.duration)
+                .rating(ratingRepository.findById(filmInfo.ratingId))
+                .genres(new TreeSet<>(genreRepository.findByIds(filmInfo.genreIds)))
+                .build();
+        film.getLikedUsers().addAll(getLikedUsersId(film.getId()));
+        return film;
+    }
+
+    private Collection<Integer> getLikedUsersId(Integer filmId) {
+        return likesRepository
+                .findAll()
+                .stream()
+                .filter(l -> l.getFilmId().equals(filmId))
+                .map(Likes::getUserId)
+                .collect(Collectors.toList());
+    }
+
+    @Value
+    private static class FilmInfo {
+        Integer id;
+        String name;
+        String description;
+        LocalDate releaseDate;
+        Integer duration;
+        Integer ratingId;
+        Set<Integer> genreIds;
+
+        public FilmInfo(Film film) {
+            id = film.getId();
+            name = film.getName();
+            description = film.getDescription();
+            releaseDate = film.getReleaseDate();
+            duration = film.getDuration();
+            if (film.getRating() == null) {
+                ratingId = null;
+            } else {
+                ratingId = film.getRating().getId();
+            }
+            genreIds = film
+                    .getGenres()
+                    .stream()
+                    .map(Genre::getId)
+                    .collect(Collectors.toSet());
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryUserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/in_memory/InMemoryUserRepository.java
@@ -1,14 +1,20 @@
 package ru.yandex.practicum.filmorate.storage.in_memory;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.model.Friends;
+import ru.yandex.practicum.filmorate.model.FriendsStatus;
+import ru.yandex.practicum.filmorate.model.Likes;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.AbstractRepository;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -16,9 +22,21 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryUserRepository implements AbstractRepository<Integer, User> {
 
-    private final Map<Integer, User> users = new ConcurrentHashMap<>();
+    private static final Comparator<User> userPopularityComparator = Comparator
+            .comparing((User user) -> user.getFriends().size()).reversed();
 
+    private final Map<Integer, User> users = new ConcurrentHashMap<>();
+    private final AbstractRepository<Likes, Likes> likesRepository;
+    private final AbstractRepository<Friends, Friends> friendsRepository;
     private Integer nextId = 1;
+
+    @Autowired
+    public InMemoryUserRepository(
+            @Qualifier("inMemoryLikesRepository") AbstractRepository<Likes, Likes> likesRepository,
+            @Qualifier("inMemoryFriendsRepository") AbstractRepository<Friends, Friends> friendsRepository) {
+        this.likesRepository = likesRepository;
+        this.friendsRepository = friendsRepository;
+    }
 
     @Override
     public User save(User user) {
@@ -38,32 +56,76 @@ public class InMemoryUserRepository implements AbstractRepository<Integer, User>
         user.getFriends().addAll(oldUser.getFriends());
         log.info("User with id={} has been updated.", user.getId());
         users.replace(user.getId(), oldUser, user);
-        return user;
+        return findById(user.getId());
     }
 
     @Override
     public User delete(User user) {
-        return users.remove(user.getId());
+        User deletedUser = findById(user.getId());
+        if (deletedUser != null) {
+            deletedUser.getFriends().forEach(id -> friendsRepository
+                    .delete(new Friends(user.getId(), id, FriendsStatus.REQUESTED)));
+            deletedUser.getLikedFilms().forEach(id -> likesRepository
+                    .delete(new Likes(user.getId(), id)));
+            users.remove(user.getId());
+        }
+        return deletedUser;
     }
 
     @Override
     public User findById(Integer userId) {
-        return users.get(userId);
+        User user = users.get(userId);
+        if (user != null) {
+            user.getFriends().addAll(getFriendsByUserId(userId, friendsRepository.findAll()));
+            user.getLikedFilms().addAll(getLikedFilmsByUserId(userId, likesRepository.findAll()));
+        }
+        return user;
     }
 
     @Override
     public Collection<User> findAll() {
-        return users.values();
+        Collection<User> userList = users.values();
+        Collection<Friends> friends = friendsRepository.findAll();
+        Collection<Likes> likes = likesRepository.findAll();
+        for (User user : userList) {
+            user.getFriends().addAll(getFriendsByUserId(user.getId(), friends));
+            user.getLikedFilms().addAll(getLikedFilmsByUserId(user.getId(), likes));
+        }
+        return userList;
     }
 
     @Override
     public Collection<User> findByIds(Collection<Integer> ids) {
-        return ids.stream().map(this::findById).collect(Collectors.toList());
+        return ids
+                .stream()
+                .map(this::findById)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<User> findFirstNTopRows(Integer n) {
-        return Collections.emptyList();
+        return findAll()
+                .stream()
+                .sorted(userPopularityComparator)
+                .limit(n)
+                .collect(Collectors.toList());
+    }
+
+    private Collection<Integer> getLikedFilmsByUserId(Integer userId, Collection<Likes> likes) {
+        return likes
+                .stream()
+                .filter(l -> l.getUserId().equals(userId))
+                .map(Likes::getFilmId)
+                .collect(Collectors.toList());
+    }
+
+    private Collection<Integer> getFriendsByUserId(Integer userId, Collection<Friends> friends) {
+        return friends
+                .stream()
+                .filter(f -> f.getUserId().equals(userId))
+                .map(Friends::getFriendId)
+                .collect(Collectors.toList());
     }
 
     private synchronized Integer getNextId() {
