38f2a4cd1ed4eb6c59764ed75a715f7b4d74fcf4
yabro <yabrow@yandex.ru>
2023-01-08 00:38:21 +0500

    feat: overriden equals & hashcode

--- src/main/java/ru/yandex/practicum/filmorate/model/Friends.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friends.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Value;
 import lombok.With;
 
+import java.util.Objects;
+
 @Value
 public class Friends {
 
@@ -12,4 +14,18 @@ public class Friends {
 
     @With
     FriendsStatus status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Friends)) return false;
+        Friends friends = (Friends) o;
+        return Objects.equals(userId, friends.userId)
+                && Objects.equals(friendId, friends.friendId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, friendId);
+    }
 }
